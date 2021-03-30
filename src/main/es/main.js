15d3a576ccf1c323524967e253c3eb9f2a2354dd
spectoor <spectoor@users.noreply.github.com>
2021-03-30 13:34:55 +0200

    Initial commit

---
+++ src/main/es/main.js
@@ -0,0 +1,11 @@
+export const fizzBuzzer = {
+    fizzBuzz(number) {
+        return /*your code here*/ undefined;
+    },
+    fizzBuzzTazz(number) {
+        return /*your code here*/ undefined;
+    },
+    fizzFizz(number) {
+        return /*your code here*/ undefined;
+    }
+};
