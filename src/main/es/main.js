945c4d21e06b6017296a0827bb3c2f9028386d5e
Guillaume Lhermitte <guillaume.lhermitte@shodo.io>
2021-03-30 15:48:39 +0200

    Le problème à changé de base

---
+++ src/main/es/main.js
@@ -0,0 +1,35 @@
+export const fizzBuzzer = {
+  fizzBuzz(nb) {
+    if (nb % 3 === 0 && nb % 5 === 0) {
+      return "FIZZBUZZ";
+    } else if (nb % 3 === 0) {
+      return "FIZZ";
+    } else if (nb % 5 === 0) {
+      return "BUZZ";
+    }
+    return nb;
+  },
+
+  fizzBuzzTazz(number) {
+    let result = fizzBuzzer.fizzBuzz(number);
+    if (result % 7 === 0) return "TAZZ";
+    if (number % 7 === 0) return result + "TAZZ";
+    return result;
+  },
+
+  fizzFizz(number) {
+    let result = number;
+    //for (let i = 0; number[i]; i++) {
+    if (number == 3) {
+      result = "FIZZ";
+    }
+    if (number % 3 == 0) {
+      if (result == number) {
+        result = "";
+      }
+      result += "FIZZ";
+    }
+    //}
+    return result;
+  }
+};
