f578f7ffbd60ad0c6e944824e1ba67828b492319
spectoor <spectoor@users.noreply.github.com>
2021-03-30 15:46:46 +0200

    tour 3

--- src/main/es/main.js
+++ src/main/es/main.js
@@ -1,30 +1,52 @@
-function addCharXTime(number, char) {
-  let str = "";
-  for (let i = 0; i !== number; arabic = arabic - 1) {
-    result = result + "I";
-  }
-  return str;
+function isNumber(numberArabic, comparaisonNumber, romanNumber)
+{
+    if (numberArabic >= comparaisonNumber)
+
 }
 
-const oneTranslator = (arabic) => {
-  let result = "";
-  if (arabic < 4 && arabic > 0)
-    for (; arabic !== 0; arabic = arabic - 1) {
-      result = result + "I";
+
+function checkNumber(result)
+{
+    if (result.arabic >= 1000) {
+        result.romanString += "M";
+        result.arabic -= 1000;
+    } else if (result.arabic >= 500) {
+        result.romanString += "D";
+        result.arabic -= 500;
+    } else if (result.arabic >= 100) {
+        result.romanString += "C";
+        result.arabic -= 100;
+    } else if (result.arabic >= 50) {
+        result.romanString += "L";
+        result.arabic -= 50;
+    } else if (result.arabic >= 10) {
+        result.romanString += "X";
+        result.arabic -= 10;
+    } else if (result.arabic >= 5) {
+        result.romanString += "V";
+        result.arabic -= 5;
+    } else if (result.arabic >= 1) {
+        result.romanString += "I";
+        result.arabic -= 1;
     }
-  if (arabic == 4) {
-    result = "IV";
-  }
-  if (arabic == 5) {
-    result = "V";
-  }
-  result += addCharXTime(4, "I");
-  return result;
-};
+}
+
+
+function arabicToRoman(arabicNumber)
+{
+    let result = {
+      arabic: arabicNumber,
+      romanString: ""
+    };
+    while (result.arabic !== 0) {
+        checkNumber(result)
+    }
+    return result.romanString
+}
 
 export const romanConverter = {
   toRoman(arabic) {
-    return oneTranslator(arabic);
+    return arabicToRoman(arabic);
   },
   fromRoman(roman) {
     return /*your code here*/ undefined;
